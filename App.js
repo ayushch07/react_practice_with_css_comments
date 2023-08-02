2182f1b70dbdd14f2ecfbd072c79071cf6c7a7dd
Ayush Chaturvedi <93607728+ayushch07@users.noreply.github.com>
2023-08-02 22:06:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import ExpenseItem from "./components/ExpenseItem.js";
+function App(){
+     return (
+                  <div><h1>Let's Start</h1>
+               <ExpenseItem/>
+               </div>
+               );
+
+    
+
+             
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,5 @@
+import ReactDom from "react-dom";
+import './index.css'
+import App from './App';
+ReactDom.render(<App />,document.getElementById('root'));
+//<App \> ki jagah hum koi element bhi add kar sakte hai jaise h1 h2 etc.
